6070f75a0973b99880c03726f704f43076505fef
AlibekovNurtilek <viruskg007@gmail.com>
2025-01-11 13:05:00 +0600

    data validation implemented

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.textSummarizer.logging import logger
 
 STAGE_NAME = "Data ingestion Stage"
@@ -6,6 +7,18 @@ STAGE_NAME = "Data ingestion Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data validation Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataValidationTrainingPipeline()
     data_ingestion.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\nx==========x")
 except Exception as e:
